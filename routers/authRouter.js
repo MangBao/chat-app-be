8fde494f865efb1aa772d6c5e190131a217dcf8d
MangBao <mangbao1301@gmail.com>
2024-02-18 21:06:43 +0700

    session login

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,79 +3,68 @@ const validateForm = require("../controllers/validateForm");
 const router = express.Router();
 const pool = require("../db");
 const bcrypt = require("bcrypt");
-// const { RESERVED_EVENTS } = require("socket.io/dist/socket")
 
-router.post("/login", async (req, res) => {
-  validateForm(req, res);
+router
+  .route("/login")
+  .get(async (req, res) => {
+    if (req.session.user && req.session.user.username) {
+      res.json({ loggedIn: true, username: req.session.user.username });
+    } else {
+      res.json({ loggedIn: false });
+    }
+  })
+  .post(async (req, res) => {
+    validateForm(req, res);
 
-  const potentialLogin = await pool.query(
-    "SELECT id, username, passhash FROM users u where u.username=$1",
-    [req.body.username]
-  );
-
-  if (potentialLogin.rowCount > 0) {
-    const isSamePass = bcrypt.compare(
-      req.body.password,
-      potentialLogin.rows[0].passhash
+    const potentialLogin = await pool.query(
+      "SELECT id, username, passhash FROM users u WHERE u.username=$1",
+      [req.body.username]
     );
 
-    if (isSamePass) {
-      //login
-      req.session.user = {
-        username: req.body.username,
-        id: potentialLogin.rows[0].id,
-      };
+    if (potentialLogin.rowCount > 0) {
+      const isSamePass = await bcrypt.compare(
+        req.body.password,
+        potentialLogin.rows[0].passhash
+      );
+      if (isSamePass) {
+        req.session.user = {
+          username: req.body.username,
+          id: potentialLogin.rows[0].id,
+        };
+        res.json({ loggedIn: true, username: req.body.username });
+      } else {
+        res.json({ loggedIn: false, status: "Wrong username or password!" });
+        console.log("not good");
+      }
     } else {
-      //bad login
-      res.json({
-        loggedIn: false,
-        status: "Wrong username or password!",
-      });
+      console.log("not good");
+      res.json({ loggedIn: false, status: "Wrong username or password!" });
     }
-  } 
-  else {
-    console.log(req);
-  }
-});
+  });
 
 router.post("/signup", async (req, res) => {
   validateForm(req, res);
 
   const existingUser = await pool.query(
-    "SELECT username FROM users WHERE username=$1",
+    "SELECT username from users WHERE username=$1",
     [req.body.username]
   );
 
   if (existingUser.rowCount === 0) {
-    //register
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    try {
-      const newUser = await pool.query(
-        "INSERT INTO users (username, passhash) VALUES ($1, $2) RETURNING id, username",
-        [req.body.username, hashedPassword]
-      );
-      console.log(newUser);
-      req.session.user = {
-        username: req.body.username,
-        id: newUser.rows[0].id,
-      };
-      res.json({
-        loggedIn: true,
-        username: req.body.username,
-      });
-    } catch (err) {
-      console.error("Error registering user:", err);
-      res.status(500).json({
-        loggedIn: false,
-        status: "Error registering user",
-      });
-    }
+    // register
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const newUserQuery = await pool.query(
+      "INSERT INTO users(username, passhash) values($1,$2) RETURNING id, username",
+      [req.body.username, hashedPass]
+    );
+    req.session.user = {
+      username: req.body.username,
+      id: newUserQuery.rows[0].id,
+    };
+
+    res.json({ loggedIn: true, username: req.body.username });
   } else {
-    res.status(400).json({
-      loggedIn: false,
-      status: "Username taken",
-    });
+    res.json({ loggedIn: false, status: "Username taken" });
   }
 });
 
